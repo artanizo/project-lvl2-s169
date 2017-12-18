a613501ff2bf6fb6b2063c10d3220f168898b983
Irina Fomina <perk.jerk@gmail.com>
2017-12-18 12:22:49 +0300

    init commit

---
+++ src/bin/gendiff.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+import util from 'commander';
+import gendiff from '..';
+
+util.version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'Output format')
+  .arguments('<firstConfig> <secondConfig>')
+  .action((first, second) => gendiff(first, second))
+  .parse(process.argv);
+
---
+++ src/index.js
@@ -0,0 +1,3 @@
+export default (firstConfig, secondConfig) => {
+  console.log(`${firstConfig} ${secondConfig}`);
+};
